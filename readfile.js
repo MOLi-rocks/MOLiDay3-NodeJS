efc876702a636cd4930eff9d58db8ff8de683b0e
Sky.Huang <kenornotes@gmail.com>
2015-11-18 02:57:54 +0800

    typo

--- readfile.js
+++ readfile.js
@@ -5,7 +5,7 @@ http.createServer(function (request, response) {
 	response.writeHead(200);
 	fs.readFile('index.html', function (error, content) {
 		response.write(content, {
-			Content - Type: 'text/html'
+			'Content-Type': 'text/html'
 		});
 		response.end();
 	});
