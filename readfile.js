9b065d2c46efcd0a9fff35c8cab9c94e874891af
Sky.Huang <kenornotes@gmail.com>
2015-11-18 01:52:01 +0800

    samples

---
+++ readfile.js
@@ -0,0 +1,10 @@
+var http = require('http');
+var fs = require('fs');
+
+http.createServer(function(request, response){
+	response.writeHead(200);
+	fs.readFile('index.html', function(error, content) {
+		response.write(content);
+		response.end();
+	});
+}).listen(9527);
